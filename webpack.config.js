2a76260026e6ddd8be3d9845fba519d4e617b094
alex <emal@damain.com>
2022-09-19 15:34:59 +0000

    fix: del publickPath

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ const config = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "build"),
-    publicPath: "/",
+    // publicPath: "/",
   },
   devServer: {
     open: true,
