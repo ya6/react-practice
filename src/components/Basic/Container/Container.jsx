d86ba8a98b39c06b89bdc084908ecf1e2fddd946
alex <emal@damain.com>
2022-09-26 20:23:16 +0000

    refactor and clean : Ditonary page

---
+++ src/components/Basic/Container/Container.jsx
@@ -0,0 +1,5 @@
+const Container = ({ children }) => {
+  return <div style={{maxWidth:"1260px", margin: "0 auto"}}>{children}</div>;
+};
+
+export default Container
---
+++ src/components/Basic/Spinner/Spinner.jsx
@@ -0,0 +1,7 @@
+import {  Spin } from "antd";
+
+const Spinner = () => {
+    return (<Spin style={{ position: "fixed", top: "40%", left: "50%", zIndex: 1000 }} size="large" />)
+} 
+
+export default Spinner
