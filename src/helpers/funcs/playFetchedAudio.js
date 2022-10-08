db133dee3387c16b67b50e1aa3ce871aea12e053
alex <emal@damain.com>
2022-10-08 14:44:25 +0000

    feat: add webkitAudioContext

--- src/helpers/funcs/playFetchedAudio.js
+++ src/helpers/funcs/playFetchedAudio.js
@@ -1,5 +1,13 @@
 const playFetchedAudio = async (url) => {
-  const ctx = new AudioContext();
+  let ctx=null
+  if('webkitAudioContext' in window) {
+    console.log("webkitAudioContext");   
+      ctx = new webkitAudioContext();
+} else {
+  console.log("AudioContext");   
+   ctx = new AudioContext();
+}
+
   try {
     const response = await fetch(url);
     const buffer = await response.arrayBuffer();
